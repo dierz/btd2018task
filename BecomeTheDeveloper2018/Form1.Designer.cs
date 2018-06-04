7e05568f576d75caf4e4f065e22c7254b8091bdc
dierz <idead09@gmail.com>
2018-06-04 17:00:31 +0300

    project vs2017comm

---
+++ BecomeTheDeveloper2018/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BecomeTheDeveloper2018
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) 
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(304, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(403, 398);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "file1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "file2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Loaded";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Loaded";
+            this.label4.Visible = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(25, 183);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(254, 227);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Analyze";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 422);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BecomeTheDeveloper2018/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BecomeTheDeveloper2018
+{
+    public partial class Form1 : Form
+    {
+        public String file1name=String.Empty, file2name=String.Empty;
+        public List<String> file1names;
+        public List<String> file2names;
+        public int c = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            checkFileStatus();
+            file2names = new List<String>();
+            file1names = new List<String>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "C:\\Users\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file1name = openFileDialog1.FileName;
+            }
+            checkFileStatus();
+            richTextBox2.Text += "file1 has loaded\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.InitialDirectory = "C:\\Users\\";
+            openFileDialog2.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog2.FilterIndex = 2;
+            openFileDialog2.RestoreDirectory = true;
+
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file2name = openFileDialog2.FileName;
+            }
+            checkFileStatus();
+            richTextBox2.Text += "file2 has loaded\n";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            if (file2name == String.Empty)
+            {
+                richTextBox2.Text += "file2 is not loaded. Error!\n";
+                return;
+            }
+            String[] lines2 = File.ReadAllLines(file2name);
+            int cnt = lines2.Length;
+            Analyze2(lines2, cnt);
+            lines2 = null;
+            if (file1name == String.Empty)
+            {
+                richTextBox2.Text += "file1 is not loaded. Error!\n";
+                return;
+            }
+            String[] lines1 = File.ReadAllLines(file1name);
+            cnt = lines1.Length;
+            Analyze1(lines1, cnt);
+            foreach(String s in file1names)
+            {
+                richTextBox1.Text += s + "\n";
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            richTextBox2.Text += elapsedTime + "\n";
+        }
+
+
+        public void checkFileStatus()
+        {
+            if (file1name.Equals(String.Empty))
+            {
+                label3.Visible = false;
+            }
+            else
+            {
+                label3.Visible = true;
+            }
+            if (file2name.Equals(String.Empty))
+            {
+                label4.Visible = false;
+            }
+            else
+            {
+                label4.Visible = true;
+            }
+        }
+        public void FindOccurences2(string line)
+        {
+            String ph;
+            String pattern = @"(user:)\s(\w+)";
+            Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match m = r.Match(line);
+            while (m.Success)
+            {
+                Group g = m.Groups[2];
+                ph = g.Value;
+                if (!IsFile2NameInList(ph))
+                {
+                    file2names.Add(ph);
+                    //richTextBox2.Text += ph + "\n";
+                }
+                m=m.NextMatch();
+            }
+        }
+        public void FindOccurences1(string line)
+        {
+            String ph;
+            String pattern = @"(user:)\s(\w+)";
+            Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match m = r.Match(line);
+            while (m.Success)
+            {
+                Group g = m.Groups[2];
+                ph = g.Value;
+                if (!IsFile2NameInList(ph)&&!IsFile1NameInList(ph))
+                {
+                    file1names.Add(ph);
+                }
+                m = m.NextMatch();
+            }
+        }
+        public bool IsFile2NameInList(string name)
+        {
+            if (file2names.Contains(name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsFile1NameInList(string name)
+        {
+            if (file1names.Contains(name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Analyze2(String[] lines2, int cnt)
+        {
+            for (c = 0; c < cnt; c++)
+            {
+                FindOccurences2(lines2.ElementAt(c));
+            }
+        }
+        public void Analyze1(string[] lines1, int cnt)
+        {
+            for (c = 0; c < cnt; c++)
+            {
+                FindOccurences1(lines1.ElementAt(c));
+            }
+        }
+    }
+}
